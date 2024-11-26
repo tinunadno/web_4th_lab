a586813816ecf44792caf4802d4c5eef491b76b1
yura <perminov1901@bk.ru>
2024-11-26 23:29:34 +0300

    front is ready

---
+++ src/main/java/org/web_4th_lab/web_4th_lab/Beans/PointService.java
@@ -0,0 +1,8 @@
+package org.web_4th_lab.web_4th_lab.Beans;
+
+import jakarta.ejb.Stateless;
+
+@Stateless
+public class PointService {
+
+}
---
+++ src/main/java/org/web_4th_lab/web_4th_lab/entities/Result.java
@@ -0,0 +1,4 @@
+package org.web_4th_lab.web_4th_lab.entities;
+
+public class Result {
+}
