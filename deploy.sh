df8d98e2fcfb906c12711a7248bc35fb5550f384
yura <perminov1901@bk.ru>
2024-12-19 10:43:13 +0300

    added readme

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-mvn clean
-mvn package
-cp target/*.war /home/yura/Applications/wildfly/wildfly-34.0.0.Beta1/standalone/deployments
--- wildfly_init.sh
+++
@@ -1,1 +0,0 @@
-bash /home/yura/Applications/wildfly/wildfly-34.0.0.Beta1/bin/standalone.sh
